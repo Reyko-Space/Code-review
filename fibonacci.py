d6d251700b7445bee14956f76bc11fea2c9d5143
Camis <reyko.yamada@outlook.com>
2023-09-14 20:36:34 -0300

    Fibonacci funcionando

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+def fibonacci(n):
+    i = 0
+    j = 1
+    k = 0
+
+    sequencia = [i, j]
+
+    while True:
+        if n > sequencia[-1]:
+            k = i + j
+            sequencia.append(k)
+            i = j
+            j = k
+        elif n < sequencia[-1]:
+            return print("Esse numero nao faz parte da sequencia de fibonacci")
+        else:
+            return print(sequencia)
+
+n = int(input("Numero: "))
+fibonacci(n)
